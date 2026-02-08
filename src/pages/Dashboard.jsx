c96bdacfcced893550afddeb3213abe888391002
code-worker-me <abrian.work@gmail.com>
2026-02-08 22:54:45 +0700

    .

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default function Dashboard() {
   return (
     <div>
       Dashboard
+      <Link to="/users">To Users</Link>
     </div>
   )
 }
